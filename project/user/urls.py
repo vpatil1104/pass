e8f3ae175db1d9fc4c6a1f6b97a95553c80dd4de
vpatil0029 <102610391+vpatil0029@users.noreply.github.com>
2022-11-16 09:06:59 +0530

    first commit

---
+++ project/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+	path('', views.registerPage, name="register"),
+	path('login', views.loginPage, name="login"),  
+	path('logout', views.logoutUser, name="logout"),
+]
